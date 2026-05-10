834014922209b2b02de2b2cad3392d0916d9795a
Kent <kent.fenwick@gmail.com>
2026-05-10 19:24:28 -0400

    Diagnostic logging for companion auth flow

--- lib/maraithon_web/plugs/fetch_current_user.ex
+++ lib/maraithon_web/plugs/fetch_current_user.ex
@@ -7,6 +7,8 @@ defmodule MaraithonWeb.Plugs.FetchCurrentUser do
 
   alias Maraithon.Accounts
 
+  require Logger
+
   @session_key "user_session_token"
 
   def init(opts), do: opts
@@ -16,16 +18,33 @@ defmodule MaraithonWeb.Plugs.FetchCurrentUser do
       token when is_binary(token) and token != "" ->
         case Accounts.get_user_by_session_token(token) do
           nil ->
+            log_diag(conn, "token_present_lookup_nil")
             conn
             |> delete_session(@session_key)
             |> assign(:current_user, nil)
 
           user ->
+            log_diag(conn, "ok", user.email)
             assign(conn, :current_user, user)
         end
 
       _ ->
+        log_diag(conn, "no_token")
         assign(conn, :current_user, nil)
+    end
+  end
+
+  defp log_diag(conn, outcome, email \\ nil) do
+    if String.starts_with?(conn.request_path, "/companion") or
+         String.starts_with?(conn.request_path, "/auth") do
+      Logger.info("FetchCurrentUser",
+        path: conn.request_path,
+        query: conn.query_string,
+        outcome: outcome,
+        email: email,
+        cookie_header_present: get_req_header(conn, "cookie") != [],
+        session_keys: get_session(conn) |> Map.keys()
+      )
     end
   end
 end
